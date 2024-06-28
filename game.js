8660526f2af04e605b2382b0ffed5ceb49874c5f
João Pedro V <mastergamerjp06@gmail.com>
2024-06-27 23:05:31 -0300

    starting developed the html and css

---
+++ game.js
@@ -0,0 +1,268 @@
+const characters = {
+    Astaroth: {
+        name: "Astaroth",
+        gender: "Homem",
+        race: "Draconato",
+        class: ["Guerreiro", "Feiticeiro"],
+        local: "Local do Acidente",
+        age: "17",
+        status: "Vivo",
+        classification: "Player",
+        token: "Astaroth.png"
+    },
+    Baltazar: {
+        name: "Baltazar",
+        description: "O Paladino",
+        token: "Baltazar.png"
+    },
+    Cadman: {
+        name: "Cadman",
+        description: "Comerciante de Pípiltin",
+        token: "Cadman.png"
+    },
+    Diego: {
+        name: "Diego",
+        description: "O Inventor da Azzip",
+        token: "Diego.png"
+    },
+    Dorian: {
+        name: "Dorian",
+        description: "O Curandeiro de Pípiltin",
+        token: "Dorian.png"
+    },
+    Hakon: {
+        name: "Hakon",
+        description: "O Mestre Místico de Pípiltin",
+        token: "Hakon.png"
+    },
+    Henri: {
+        name: "Henri",
+        description: "O Braço direito de James",
+        token: "Henri.png"
+    },
+    Irene: {
+        name: "Irene",
+        description: "A curandeira misteriosa",
+        token: "Irene.png"
+    },
+    Isabelle: {
+        name: "Isabelle",
+        description: "A barda dracônica",
+        token: "Isabelle.png"
+    },
+    Ivor: {
+        name: "Ivor",
+        description: "O mago de Piphet",
+        token: "Ivor.png"
+    },
+    JackCooper: {
+        name: "Jack Cooper",
+        description: "O caçador de Yharnam",
+        token: "Jack_Cooper.png"
+    },
+    James: {
+        name: "James",
+        description: "O capitão",
+        token: "James.png"
+    },
+    Jane: {
+        name: "Jane",
+        description: "A pirata",
+        token: "Jane.png"
+    },
+    Lina: {
+        name: "Lina",
+        description: "A ladina de ???",
+        token: "Lina.png"
+    },
+    LordeDoSubmundo: {
+        name: "Lorde do Submundo",
+        description: "O Lorde do Submundo",
+        token: "Lorde_do_submundo.png"
+    },
+    Lucas: {
+        name: "Lucas",
+        description: "O caçador de Pípiltin",
+        token: "Lucas.png"
+    },
+    Magnus: {
+        name: "Magnus",
+        description: "O Rei de Urgressus",
+        token: "Magnus.png"
+    },
+    Mary: {
+        name: "Mary",
+        description: "A pirata pistoleira",
+        token: "Mary.png"
+    },
+    Matiza: {
+        name: "Matiza",
+        description: "O campeão da velha guarda",
+        token: "Matiza.png"
+    },
+    Rebeca: {
+        name: "Rebeca",
+        description: "A feiticeira",
+        token: "Rebeca.png"
+    },
+    Sabnock: {
+        name: "Sabnock",
+        description: "???",
+        token: "Sabnock.png"
+    },
+    Spamton: {
+        name: "Spamton",
+        description: "O familiar de Jack Cooper",
+        token: "Spamton.png"
+    },
+    Spike: {
+        name: "Spike",
+        description: "Demônio de Mictlan",
+        token: "Spike.png"
+    },
+    Tyrion: {
+        name: "Tyrion",
+        description: "O encantador de Pípiltin",
+        token: "Tyrion.png"
+    },
+    Willian: {
+        name: "Willian",
+        description: "O paladino misterioso",
+        token: "Willian.png"
+    },
+    Yareth: {
+        name: "Yareth",
+        description: "O mago de sangue",
+        token: "Yareth.png"
+    },
+    Zotikotita: {
+        name: "Zotikotita",
+        description: "A Entidade da Vida",
+        token: "Zotikotita.png"
+    },
+    Avna: {
+        name: "Avna",
+        description: "Os dez braços direitos do Lorde",
+        token: "Avna.png"
+    },
+    Bellus: {
+        name: "Bellus",
+        description: "A máscara de Celestia",
+        token: "Bellus.jpg"
+    },
+    Berriel: {
+        name: "Berriel",
+        description: "O demônio brincalhão",
+        token: "Berrial.png"
+    },
+    Boris: {
+        name: "Boris",
+        description: "O Capitão Brasil",
+        token: "Boris.png"
+    },
+    Luke: {
+        name: "Luke",
+        description: "O homem que não faz flexão, ele empurra a terra para baixo",
+        token: "Luke.png"
+    },
+    Maria: {
+        name: "Maria",
+        description: "A Ladina",
+        token: "Maria.png"
+    },
+    Niberius: {
+        name: "Niberius",
+        description: "O paladino da vingança",
+        token: "Pai_do_Arth.png"
+    },
+    RhannyGallahar: {
+        name: "Rhanny Gallahar",
+        description: "A prodígia de Pípiltin",
+        token: "Rhanny Gallahar.png"
+    },
+    Sif: {
+        name: "Sif",
+        description: "???",
+        token: "Sif.png"
+    },
+    Callan: {
+        name: "Callan",
+        description: "???",
+        token: "Callan.png"
+    },
+    Emma: {
+        name: "Emma",
+        description: "A Mestra Arcana de Metalurgis",
+        token: "Emma.png"
+    },
+    Volga: {
+        name: "Volga",
+        description: "O Rei dos Dragões",
+        token: "Volga.png"
+    },
+    Alduim: {
+        name: "Alduim",
+        description: "O Dragão Branco Ancião",
+        token: "Alduim.png"
+    },
+    Niti: {
+        name: "Niti",
+        description: "A Guardiã da Justiça",
+        token: "Niti.png"
+    },
+    Nayahath: {
+        name: "Nayahath",
+        description: "A Entidade do Arcanismo",
+        token: "Nayahath.png"
+    },
+    Kataha: {
+        name: "Kataha",
+        description: "A Mestra das Lâminas",
+        token: "Kataha.png"
+    },
+    Parvatam: {
+        name: "Parvatam",
+        description: "O Guardião das Montanhas",
+        token: "Parvatam.png"
+    },
+    Acalhueltecani: {
+        name: "Acalhueltecani",
+        description: "O Guia de Mictlan",
+        token: "Acalhueltecani.png"
+    },
+    Illidan: {
+        name: "Illidan",
+        description: "O elfo que finalizou o Volga",
+        token: "Illidan.png"
+    },
+    Tyrania: {
+        name: "Tyrania",
+        description: "O Demônio de Mictlan",
+        token: "Tyrania.png"
+    },
+    Laura: {
+        name: "Laura",
+        description: "A bibliotecária de Pípiltin",
+        token: "Laura.png"
+    },
+    Nosferato: {
+        name: "Nosferato",
+        description: "O alquimista de Pípiltin",
+        token: "Nosferato.png"
+    },
+    Syneidisis: {
+        name: "Syneídisis",
+        description: "O Ego",
+        token: "Syneídisis.png"
+    },
+    Joana: {
+        name: "Joana",
+        description: "A curandeira de Pípiltin",
+        token: "Joana.png"
+    },
+    Wallace: {
+        name: "Wallace",
+        description: "???",
+        token: "Wallace.png"
+    }
+}
